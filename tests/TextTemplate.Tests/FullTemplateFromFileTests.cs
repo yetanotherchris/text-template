424021f77e0325e0432fd6013a6bff7030c14b08
Chris S. <chrisasmall+github@gmail.com>
2025-06-11 17:37:36 +0100

    Use dot-prefixed field names in templates

---
+++ tests/TextTemplate.Tests/FullTemplateFromFileTests.cs
@@ -0,0 +1,30 @@
+using Xunit;
+using TextTemplate;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TextTemplate.Tests;
+
+public class FullTemplateFromFileTests
+{
+    [Fact]
+    public void AntlrTemplate_FileTemplate_AllFeatures()
+    {
+        string baseDir = AppContext.BaseDirectory;
+        string templatePath = Path.Combine(baseDir, "TestData", "full_template.txt");
+        string expectedPath = Path.Combine(baseDir, "TestData", "full_template_expected.txt");
+
+        string template = File.ReadAllText(templatePath);
+        string expected = File.ReadAllText(expectedPath);
+
+        var result = AntlrTemplate.Process(template, new Dictionary<string, object>
+        {
+            ["Name"] = "Bob",
+            ["Gift"] = "toaster",
+            ["Attended"] = false,
+            ["Items"] = new[] { "book", "pen" }
+        });
+
+        Assert.Equal(expected, result);
+    }
+}
